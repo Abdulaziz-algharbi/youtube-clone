5eeb1910816c845ec681f17a4533406f9339ea6a
abdulaziz <abdulazizalgharbi8@gmail.com>
2025-11-18 22:55:00 +0400

    Add the processing videos feature locally first

---
+++ video-processing-service/src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import ffmpeg from "fluent-ffmpeg";
+
+const app = express();
+app.use(express.json());
+
+app.post("/process-video", (req, res) => {
+  // Get the path of the input video file from the request body
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
+
+  if (!inputFilePath || !outputFilePath) {
+    res.status(400).send("Bad Request: Missing file path.");
+  }
+
+  ffmpeg(inputFilePath)
+    .outputOptions("-vf", "scale=-1:360")
+    .on("end", () => {
+      res.status(200).send("Processing Finished Successfully.");
+    })
+    .on("error", (err) => {
+      console.log(`An error occurred: ${err.message}`);
+      res.status(500).send(`Internal Server Error: ${err.message}`);
+    })
+    .save(outputFilePath);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Video Processing service listening at http://localhost:${port}`);
+});
