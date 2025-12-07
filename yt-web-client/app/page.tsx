1163100f6eaa9d46dcc4c9f4bc961ae8e4d1ae4d
abdulaziz <abdulazizalgharbi8@gmail.com>
2025-12-07 14:44:00 +0400

    Create Next App

---
+++ yt-web-client/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <div>
+        <p>
+          Get started by editing&nbsp;
+          <code>app/page.tsx</code>
+        </p>
+      </div>
+    </main>
+  );
+}
